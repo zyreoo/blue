e3e45c5d82530cd807bc32f05ab54a83fdb5183b
zyreoo <simone.marton89@gmail.com>
2025-06-17 18:39:01 -0700

    admin page

--- next.config.js
+++ next.config.js
@@ -1,21 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'plus.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'iso.500px.com',
-      },
+    domains: [
+      'images.unsplash.com',
+      'plus.unsplash.com',
+      'source.unsplash.com',
+      'iso.500px.com',
     ],
+    formats: ['image/avif', 'image/webp'],
+    minimumCacheTTL: 60 * 60 * 24, // 24 hours
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
+  compress: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  generateEtags: true,
 };
 
 module.exports = nextConfig; 
